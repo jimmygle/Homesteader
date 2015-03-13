2557f958a5ae372ba4665d32e5730bd84d771a64
Jimmy Gleason <jimmygle@gmail.com>
2015-03-12 21:52:08 -0400

    Added homestead config test and default Homestead.yaml file to test against

--- tests/ConfigHomesteadConfigTest.php
+++ tests/ConfigHomesteadConfigTest.php
@@ -1,20 +1,39 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: jimmygle
- * Date: 3/8/15
- * Time: 8:12 PM
- */
 
-use org\bovigo\vfs\vfsStreamWrapper;
-use org\bovigo\vfs\vfsStreamDirectory;
+use Homesteader\Config\HomesteadConfig;
+use org\bovigo\vfs\vfsStream;
 
-class ConfigHomesteadConfigTest {
+class ConfigHomesteadConfigTest extends \PHPUnit_Framework_TestCase {
+
+    protected $homesteadConfigFile;
 
     public function setUp()
     {
-        vfsStreamWrapper::register();
-        vfsStreamWrapper::setRoot(new vfsStreamDirectory($_SERVER['HOME'] . DIRECTORY_SEPARATOR . './homestead'));
+        $homesteadPath = '.homestead';
+        vfsStream::setup($homesteadPath);
+        $this->homesteadConfigFile = vfsStream::url("$homesteadPath/Homestead.yaml");
+        file_put_contents($this->homesteadConfigFile, file_get_contents(dirname(__FILE__) . '/DefaultHomestead.yaml'));
+    }
+
+    public function testConfigFileOpensAsString()
+    {
+        $configFile = new HomesteadConfig($this->homesteadConfigFile);
+        $this->assertStringStartsWith('---', (string) $configFile);
+        $this->assertStringStartsWith('---', $configFile->asString());
+    }
+
+    public function testConfigFileOpensAsArray()
+    {
+        $configFile = new HomesteadConfig($this->homesteadConfigFile);
+        $this->assertArrayHasKey('ip', $configFile->asArray());
+        $this->assertArrayHasKey('memory', $configFile->asArray());
+        $this->assertArrayHasKey('cpus', $configFile->asArray());
+        $this->assertArrayHasKey('authorize', $configFile->asArray());
+        $this->assertArrayHasKey('keys', $configFile->asArray());
+        $this->assertArrayHasKey('folders', $configFile->asArray());
+        $this->assertArrayHasKey('sites', $configFile->asArray());
+        $this->assertArrayHasKey('databases', $configFile->asArray());
+        $this->assertArrayHasKey('variables', $configFile->asArray());
     }
 
 } 
