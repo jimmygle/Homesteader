7f0666ba147a718ba8b2b6a174027cb12c19b63c
Jimmy Gleason <jimmygle@gmail.com>
2015-03-15 00:32:24 -0400

    Removing shortened versions of config commands until abstraction happens

--- src/Config/ConfigNewCommand.php
+++ src/Config/ConfigNewCommand.php
@@ -18,12 +18,12 @@ class ConfigNewCommand extends ConfigCommand {
 		$this->setName('config:new');
 		$this->setDescription('Add a new item to the Homestead config.');
 		$this->addArgument('key', InputArgument::OPTIONAL, 'Type of config to add. [folder|site|database|variable]');
-	    $this->addOption('host', 'o', InputArgument::OPTIONAL, 'Path on your local machine.');
-        $this->addOption('homestead', 'g', InputArgument::OPTIONAL, 'Path in Homestead\'s filesystem.');
-        $this->addOption('domain', 'd', InputArgument::OPTIONAL, 'Local domain name of site.');
-        $this->addOption('key', 'k', InputArgument::OPTIONAL, 'Name of variable.');
-        $this->addOption('value', 'a', InputArgument::OPTIONAL, 'Value of variable.');
-        $this->addOption('name', 'b', InputArgument::OPTIONAL, 'Database name.');
+	    $this->addOption('host', null, InputArgument::OPTIONAL, 'Path on your local machine.');
+        $this->addOption('homestead', null, InputArgument::OPTIONAL, 'Path in Homestead\'s filesystem.');
+        $this->addOption('domain', null, InputArgument::OPTIONAL, 'Local domain name of site.');
+        $this->addOption('key', null, InputArgument::OPTIONAL, 'Name of variable.');
+        $this->addOption('value', null, InputArgument::OPTIONAL, 'Value of variable.');
+        $this->addOption('name', null, InputArgument::OPTIONAL, 'Database name.');
     }
 
     /**
@@ -175,19 +175,19 @@ class ConfigNewCommand extends ConfigCommand {
         $this->output->writeln('  Available options are included with example values.');
         $this->output->writeln('');
         $this->output->writeln('  <comment>config:new folder</comment>       Adds synced directory set');
-        $this->output->writeln('    <info>--host</info>      (-h)      /path/to/host/dir');
-        $this->output->writeln('    <info>--homestead</info> (-g)      /path/on/homestead/to/dir');
+        $this->output->writeln('    <info>--host</info>                /path/to/host/dir');
+        $this->output->writeln('    <info>--homestead</info>           /path/on/homestead/to/dir');
         $this->output->writeln('');
         $this->output->writeln('  <comment>config:new site</comment>         Adds site and directory');
-        $this->output->writeln('    <info>--domain</info>    (-d)      domaintoadd.com');
-        $this->output->writeln('    <info>--homestead</info> (-g)      /path/on/homestead/to/public/');
+        $this->output->writeln('    <info>--domain</info>              domaintoadd.com');
+        $this->output->writeln('    <info>--homestead</info>           /path/on/homestead/to/public/');
         $this->output->writeln('');
         $this->output->writeln('  <comment>config:new variable</comment>     Adds variable');
-        $this->output->writeln('    <info>--key</info>       (-k)      VAR');
-        $this->output->writeln('    <info>--value</info>     (-v)      VALUE');
+        $this->output->writeln('    <info>--key</info>                 VAR');
+        $this->output->writeln('    <info>--value</info>               VALUE');
         $this->output->writeln('');
         $this->output->writeln('  <comment>config:new database</comment>     Adds database');
-        $this->output->writeln('    <info>--name</info>      (-n)      db_name');
+        $this->output->writeln('    <info>--name</info>                db_name');
         $this->output->writeln('');
         $this->output->writeln('  Add -n for no interaction.');
         $this->output->writeln('');
