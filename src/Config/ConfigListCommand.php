f12fc9076fe7cc02940e025206b9573e60182f4c
Jimmy Gleason <jimmygle@gmail.com>
2015-03-14 23:42:03 -0400

    Changed config:list to not load config file so it can show the list if config file is not found

--- src/Config/ConfigListCommand.php
+++ src/Config/ConfigListCommand.php
@@ -28,35 +28,33 @@ class ConfigListCommand extends ConfigCommand {
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        parent::execute($input, $output);
-
-        $this->output->writeln('');
-        $this->output->writeln('The config commands allow for easy manipulation of the Homestead config file.');
-        $this->output->writeln('');
-        $this->output->writeln('  <comment>config:show</comment>               Outputs the current config file to terminal.');
-        $this->output->writeln('');
-        $this->output->writeln('  All config:new options are optional. You\'ll be prompted unless -n is set.');
-        $this->output->writeln('');
-        $this->output->writeln('  <comment>config:new folder</comment>         Adds synced directory set.');
-        $this->output->writeln('    <info>--host</info>           (-o)     Path on your local system.');
-        $this->output->writeln('    <info>--homestead</info>      (-g)     Path in Homestead\'s filesystem.');
-        $this->output->writeln('');
-        $this->output->writeln('  <comment>config:new site</comment>           Adds site and directory.');
-        $this->output->writeln('    <info>--domain</info>         (-d)     Local domain name of site.');
-        $this->output->writeln('    <info>--homestead</info>      (-g)     Path to public directory in Homestead.');
-        $this->output->writeln('');
-        $this->output->writeln('  <comment>config:new variable</comment>       Adds system variables.');
-        $this->output->writeln('    <info>--key</info>            (-k)      Name of variable.');
-        $this->output->writeln('    <info>--value</info>          (-a)      Value of variable.');
-        $this->output->writeln('');
-        $this->output->writeln('  <comment>config:new database</comment>       Adds database.');
-        $this->output->writeln('    <info>--name</info>           (-b)      Database name.');
-        $this->output->writeln('');
-        $this->output->writeln('  <comment>Global Options:</comment>');
-        $this->output->writeln('    <info>--no-interaction</info> (-n)   No interaction (for unattended use).');
-        $this->output->writeln('    <info>--file</info>           (-f)   Custom config file path.');
-        $this->output->writeln('');
-        $this->output->writeln('');
+        $output->writeln('');
+        $output->writeln('The config commands allow for easy manipulation of the Homestead config file.');
+        $output->writeln('');
+        $output->writeln('  <comment>config:show</comment>               Outputs the current config file to terminal.');
+        $output->writeln('');
+        $output->writeln('  All config:new options are optional. You\'ll be prompted unless -n is set.');
+        $output->writeln('');
+        $output->writeln('  <comment>config:new folder</comment>         Adds synced directory set.');
+        $output->writeln('    <info>--host</info>           (-o)     Path on your local system.');
+        $output->writeln('    <info>--homestead</info>      (-g)     Path in Homestead\'s filesystem.');
+        $output->writeln('');
+        $output->writeln('  <comment>config:new site</comment>           Adds site and directory.');
+        $output->writeln('    <info>--domain</info>         (-d)     Local domain name of site.');
+        $output->writeln('    <info>--homestead</info>      (-g)     Path to public directory in Homestead.');
+        $output->writeln('');
+        $output->writeln('  <comment>config:new variable</comment>       Adds system variables.');
+        $output->writeln('    <info>--key</info>            (-k)      Name of variable.');
+        $output->writeln('    <info>--value</info>          (-a)      Value of variable.');
+        $output->writeln('');
+        $output->writeln('  <comment>config:new database</comment>       Adds database.');
+        $output->writeln('    <info>--name</info>           (-b)      Database name.');
+        $output->writeln('');
+        $output->writeln('  <comment>Global Options:</comment>');
+        $output->writeln('    <info>--no-interaction</info> (-n)   No interaction (for unattended use).');
+        $output->writeln('    <info>--file</info>           (-f)   Custom config file path.');
+        $output->writeln('');
+        $output->writeln('');
     }
 
 }
