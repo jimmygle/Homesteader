76f55f55258600c6859404a079dad124567f4dc3
Jimmy Gleason <jimmygle@gmail.com>
2015-03-14 21:25:31 -0400

    Added test for config list command

---
+++ tests/ConfigListCommandTest.php
@@ -0,0 +1,21 @@
+<?php require_once __DIR__.'/ConfigSetup.php';
+
+use Symfony\Component\Console\Tester\CommandTester;
+use Symfony\Component\Console\Application;
+use Homesteader\Config\ConfigListCommand;
+
+class ConfigListCommandTest extends ConfigSetup {
+
+    public function testItShouldOutputListOfOptions()
+    {
+        $application = new Application();
+        $application->add(new ConfigListCommand());
+
+        $command = $application->find('config');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute([]);
+
+        $this->assertStringStartsWith(PHP_EOL . 'The config commands allow for easy manipulation of the Homestead config file.', $commandTester->getDisplay());
+    }
+
+}
