1d71c429bb7be77c35480d30bd32524d94a0b2a0
Jimmy Gleason <jimmygle@gmail.com>
2015-03-14 20:48:13 -0400

    Help text for config:new

--- src/Config/ConfigNewCommand.php
+++ src/Config/ConfigNewCommand.php
@@ -1,5 +1,6 @@
 <?php namespace Homesteader\Config;
 
+use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -16,7 +17,7 @@ class ConfigNewCommand extends ConfigCommand {
         parent::configure();
 		$this->setName('config:new');
 		$this->setDescription('Add a new item to the Homestead config.');
-		$this->addArgument('key', InputArgument::OPTIONAL, 'Type of config to add.');
+		$this->addArgument('key', InputArgument::OPTIONAL, 'Type of config to add. [folder|site|database|variable]');
 	}
 
     /**
@@ -42,11 +43,16 @@ class ConfigNewCommand extends ConfigCommand {
 			case 'database':
 				$this->newDatabase();
 				break;
-			case ('variable'):
+			case 'variable':
 				$this->newVariable();
 				break;
+            case 'list':
+                $this->listKeys();
+                break;
 			default:
-				$this->output->writeln('<error>Invalid entry!</error>');
+				$helpCommand = $this->getApplication()->find('config:new');
+                $input = new ArrayInput(['key' => 'list']);
+                $helpCommand->run($input, $output);
 				break;
 		}
 	}
@@ -158,5 +164,31 @@ class ConfigNewCommand extends ConfigCommand {
 
 		$this->homesteadConfigSave();
 	}
+
+    protected function listKeys()
+    {
+        $this->output->writeln('');
+        $this->output->writeln('Adds new config options to the Homestead config file.');
+        $this->output->writeln('');
+        $this->output->writeln('  Available options are included with example values.');
+        $this->output->writeln('');
+        $this->output->writeln('  <comment>config:new folder</comment>       Adds synced directory set');
+        $this->output->writeln('    <info>--host</info>      (-h)      /path/to/host/dir');
+        $this->output->writeln('    <info>--homestead</info> (-g)      /path/on/homestead/to/dir');
+        $this->output->writeln('');
+        $this->output->writeln('  <comment>config:new site</comment>         Adds site and directory');
+        $this->output->writeln('    <info>--domain</info>    (-d)      domaintoadd.com');
+        $this->output->writeln('    <info>--homestead</info> (-g)      /path/on/homestead/to/public/');
+        $this->output->writeln('');
+        $this->output->writeln('  <comment>config:new variable</comment>     Adds variable');
+        $this->output->writeln('    <info>--key</info>       (-k)      VAR');
+        $this->output->writeln('    <info>--value</info>     (-v)      VALUE');
+        $this->output->writeln('');
+        $this->output->writeln('  <comment>config:new database</comment>     Adds database');
+        $this->output->writeln('    <info>--name</info>      (-n)      db_name');
+        $this->output->writeln('');
+        $this->output->writeln('  Add -n for no interaction.');
+        $this->output->writeln('');
+    }
 
 }
